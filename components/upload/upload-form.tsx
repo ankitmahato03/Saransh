3879ae1079915b7037212ee4557ea655350b72e6
ankitmahato03 <ankitmahato1212@gmail.com>
2025-07-10 22:58:26 +0530

    Upload page changed and add zod Validation on file submitting

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,11 +1,38 @@
-import { Button } from "../ui/button";
+"use client";
 
-export default function UploadForm(){
-    return (
-        <div><form className="flex flex-col gap-6">
-                <input type="file" className=""/>
-                <Button>Upload Your PDF</Button>
-        </form>
-        </div>
+import UploadFormInput from "./upload-form-input";
+import { file, z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "invalid File Type " })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File Must be less than 20MB"
     )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File Must be a"
+    ),
+});
+
+export default function UploadForm() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    const validatedFeilds = schema.safeParse(file);
+    if (!validatedFeilds.success) {
+      console.log(
+        validatedFeilds.error.flatten().fieldErrors.file?.[0] ?? "Invalid File"
+      );
+    }
+  };
+  return (
+    <div className="flex flex-col gap-8 mx-auto max-w-2xl w-full">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
 }
