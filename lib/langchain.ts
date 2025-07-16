a5e7428818e8316b2b5b604524bc9851657c4490
ankitmahato03 <ankitmahato1212@gmail.com>
2025-07-16 23:04:27 +0530

    aise hi

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,19 +1,33 @@
-// import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+// // import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+// import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
 
 
 
 
-export async function  fetchAndExtractPdfText(pdfUrl:String){
-    const response = await fetch(pdfUrl)
+// export async function  fetchAndExtractPdfText(pdfUrl:String){
+//     const response = await fetch(pdfUrl)
 
-const blob= await response.blob();
-const arrayBuffer = await blob.arrayBuffer();
-const loader = new PDFLoader(new Blob([arrayBuffer]));
-const docs = await loader.load();
+// const blob= await response.blob();
+// const arrayBuffer = await blob.arrayBuffer();
+// const loader = new PDFLoader(new Blob([arrayBuffer]));
+// const docs = await loader.load();
 
-return docs.map((doc)=>doc.pageContent).join('\n')
+// return docs.map((doc)=>doc.pageContent).join('\n')
 
 
 
+// }
+
+
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+export async function fetchAndExtractPdfText(pdfUrl: string) {
+  const response = await fetch(pdfUrl);
+  const blob = await response.blob();
+  const arrayBuffer = await blob.arrayBuffer();
+  
+  const loader = new PDFLoader(new Blob([arrayBuffer]));
+  const docs = await loader.load();
+
+  return docs.map((doc) => doc.pageContent).join('\n');
 }
